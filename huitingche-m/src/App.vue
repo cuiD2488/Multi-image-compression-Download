b636b5b6abf0ba9dc547b2bfe244078d7f724f07
cuiD2488 <1029124915@qq.com>
2019-03-11 09:59:34 +0800

    u

--- huitingche-m/src/App.vue
+++ huitingche-m/src/App.vue
@@ -12,7 +12,7 @@ export default {
 
 <style lang="less">
 @import '~vux/src/styles/reset.less';
-@import './style/publick.css';
+// @import './style/publick.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- huitingche-m/src/main.js
+++ huitingche-m/src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import store from './store'
 import VueAMap from 'vue-amap'
 import 'lib-flexible/flexible.js'
-// import '@/style/publick.css'
+import '@/style/publick.css'
 import { WechatPlugin, ToastPlugin, LoadingPlugin, ConfirmPlugin, Toast } from 'vux'
 
 Vue.config.productionTip = false
