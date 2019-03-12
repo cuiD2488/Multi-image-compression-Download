7baf32b992d946a4d22f651f34b289993e15a6e4
945726616 <945726616@qq.com>
2019-03-12 11:51:08 +0800

    停车记录页面提交 11:51

--- huitingche-m/src/iviews/mine/parkingRecord/index.vue
+++ huitingche-m/src/iviews/mine/parkingRecord/index.vue
@@ -9,43 +9,38 @@
     <!-- 停车记录 -->
     <div class="parkingRecordContent">
       <div v-for="(item, index) in recordList" :key="index">
-        <div class="location">{{item.location}}</div>
+        <div class="location">{{item.city + item.county + item.detailedAddress + item.parkingLotName}}</div>
         <div class="date">
-          <span>{{item.date}}</span>
-          <span>{{item.begingTime}}至{{item.endTime}}</span>
+          <span>停车时间：</span>
+          <span>{{item.enterTime}} 至 {{item.outTime}}</span>
         </div>
-        <div class="stopTime">{{item.stopTime}}</div>
+        <div class="date">
+          <div>预付金额：<span style="color:#000">{{item.orderMoney}}元</span></div>
+          <div>实付金额：<span style="color:#000">{{item.actualPayMoney}}元</span></div>
+        </div>
+        <div class="delLine">
+          <div>{{item.orderStatus === 0 ? '待支付' : item.orderStatus === 1 ? '支付成功' : '待补缴'}}</div>
+          <div class="deleteDiv">
+            <x-button @click.native="sendCode" type="primary" mini text="删除" class="deleteBtn"></x-button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 import {ApiQueryPkOrder} from '@/api'
-import { Tab, TabItem } from 'vux'
+import { Tab, TabItem, XButton } from 'vux'
 
 export default {
   components: {
     Tab,
-    TabItem
+    TabItem,
+    XButton
   },
   data () {
     return {
-      recordList: [
-        // {
-        //   location: '深圳市宝安区西乡街道银田工业区停车场',
-        //   date: '2019-02-16',
-        //   begingTime: '10:00',
-        //   endTime: '13:30',
-        //   stopTime: '总停留3小时30分钟'
-        // },
-        // {
-        //   location: '深圳市宝安区西乡街道银田工业区停车场',
-        //   date: '2019-02-16',
-        //   begingTime: '10:00',
-        //   endTime: '13:30',
-        //   stopTime: '总停留3小时30分钟'
-        // }
-      ]
+      recordList: []
     }
   },
   methods: {
@@ -61,6 +56,8 @@ export default {
       if (res.code === 200) {
         let data = res.data.data
         for (let index in data) {
+          data[index].enterTime = data[index].enterTime.substring(0, data[index].enterTime.length - 5)
+          data[index].outTime = data[index].outTime.substring(0, data[index].outTime.length - 5)
           this.recordList.push(data[index])
         }
       }
@@ -74,19 +71,34 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-  .parkingRecordContent{
-    text-align: left;
-    padding: 20px 0;
-    font-size: 28px;
-    >div {
-      margin-bottom: 20px;
-      background: #fff;
-      padding: 40px 30px;
-    }
-    .date{
-      display: flex;
-      justify-content: space-between;
-      color: #898989;
+.parkingRecordContent{
+  text-align: left;
+  padding: 20px 0;
+  font-size: 28px;
+  >div {
+    margin-bottom: 20px;
+    background: #fff;
+    padding: 40px 30px;
+  }
+  .date{
+    display: flex;
+    justify-content: space-between;
+    color: #898989;
+    font-size: 24px;
+  }
+}
+.delLine{
+  display: flex;
+  padding-top: 20px;
+  line-height: 54px;
+  // justify-content: space-between;
+  .deleteDiv{
+    flex-grow: 1;
+    position: relative;
+    .deleteBtn{
+      position: absolute;
+      right: 0;
     }
   }
+}
 </style>
