aa5146f859c71b035935015c739eb06c2e21da97
945726616 <945726616@qq.com>
2019-04-02 14:17:40 +0800

    停车场登录控制

--- huitingche-admin/src/main.js
+++ huitingche-admin/src/main.js
@@ -12,6 +12,28 @@ Vue.use(iView)
 Vue.prototype.utils = utils
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start()
+  window.document.title = to.meta.title || '云尚停车商户管理'
+  if (!sessionStorage.getItem('userInfo') && to.name !== 'login') {
+    iView.Message.info('请先登录')
+    next({
+      name: 'login'
+    })
+  } else {
+    let breadcrumbList = []
+    if (to.meta.parent) {
+      let route = router.options.routes[0].children.find((n) => {
+        return n.name === to.meta.parent
+      })
+      breadcrumbList = [route]
+    }
+    breadcrumbList = [...breadcrumbList, ...to.matched.slice(1)]
+    store.commit('SET_BREADCRUMB', breadcrumbList)
+    next()
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
